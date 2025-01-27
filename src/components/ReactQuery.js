b312a508b8fc2236417dd5cbced0f59ae813d2c8
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-01-27 21:41:36 +0530

    Update ReactQuery.js

--- src/components/ReactQuery.js
+++ src/components/ReactQuery.js
@@ -1,9 +1,50 @@
-import React from 'react'
+// import React from 'react'
 
-const ReactQuery = () => {
+// const ReactQuery = () => {
+//   return (
+//     <div>ReactQuery</div>
+//   )
+// }
+
+// export default ReactQuery
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
+
+// Create a client
+const queryClient = new QueryClient();
+
+// Fetch function
+const fetchData = async () => {
+  const response = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  return response.data;
+};
+
+const Posts = () => {
+  const { data, isLoading, isError, error } = useQuery(["posts"], fetchData);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <div>Error: {error.message}</div>;
+
   return (
-    <div>ReactQuery</div>
-  )
-}
+    <div>
+      <h1>Posts</h1>
+      <ul>
+        {data.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default ReactQuery
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Posts />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
