06167cfef088a2bd0179c876d93029cac50e874c
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-01-27 21:36:17 +0530

    Update Axios.js

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import  Axios form 'axios'
+// import  Axios form 'axios'
+const url='https://www.example.com'
 const Axios = () => {
-  
+  const fecthing = axios.get(url)
   return (
     <div>
     Axios
